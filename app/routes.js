f67a43ac721250a1e3a3463c73e4700eaf873b48
Andreea Vlad <a.vlad@nhs.net>
2020-08-10 17:16:07 +0100

    p

--- app/routes.js
+++ app/routes.js
@@ -292,5 +292,150 @@ router.post('/main-or-dependant-v2', function(req,res){
 
 })
 
+//v3
+
+router.post('/ihs-question-v3', function(req,res){
+    var contacted = req.session.data['ihs-question-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/work-question')
+    }
+    else {
+        res.redirect('/v3/not-entitled')
+    }
+
+})
+
+router.post('/work-question-v3', function(req,res){
+    var contacted = req.session.data['work-question-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/6-months')
+    }
+    else {
+        res.redirect('/v3/not-entitled')
+    }
+
+})
+
+router.post('/6-months-v3', function(req,res){
+    var contacted = req.session.data['6-months-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/average-time')
+    }
+    else {
+        res.redirect('/v3/not-entitled')
+    }
+
+})
+
+router.post('/average-time-v3', function(req,res){
+    var contacted = req.session.data['average-time-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/declaration')
+    }
+    else {
+        res.redirect('/v3/not-entitled')
+    }
+
+})
+
+router.post('/nhs-question-v3', function(req,res){
+    var contacted = req.session.data['nhs-question-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/nhs-path')
+    }
+    else {
+        res.redirect('/v3/non-nhs-path')
+    }
+
+})
+
+router.post('/another-job-v3', function(req,res){
+    var contacted = req.session.data['another-job-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/another-job')
+    }
+    else {
+        res.redirect('/v3/telephone-number')
+    }
+
+})
+
+router.post('/non-nhs-path-another-job-v3', function(req,res){
+    var contacted = req.session.data['non-nhs-path-another-job-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/non-nhs-path-another-job')
+    }
+    else {
+        res.redirect('/v3/check-your-answers-nhs-path')
+    }
+
+})
+
+router.post('/attachment-decision-v3', function(req,res){
+    var contacted = req.session.data['attachment-decision-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/attachments-add')
+    }
+    else {
+        res.redirect('/v3/attachments')
+    }
+
+})
+
+router.post('/attachments-add-v3', function(req,res){
+    var contacted = req.session.data['attachments-add-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/attachments2')
+    }
+    else {
+        res.redirect('/v3/check-your-answers-nhs-path')
+    }
+
+})
+
+router.post('/dependant-question-v3', function(req,res){
+    var contacted = req.session.data['dependant-question-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/dependant-details')
+    }
+    else {
+        res.redirect('/v3/non-nhs-path')
+    }
+
+})
+
+router.post('/dependant-details-v3', function(req,res){
+    var contacted = req.session.data['dependant-details-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/dependant-details2')
+    }
+    else {
+        res.redirect('/v3/non-nhs-path')
+    }
+
+})
+
+router.post('/dependant-details2-v3', function(req,res){
+    var contacted = req.session.data['dependant-details2-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/dependant-details2')
+    }
+    else {
+        res.redirect('/v3/non-nhs-path')
+    }
+
+})
+
+router.post('/main-or-dependant-v3', function(req,res){
+    var contacted = req.session.data['main-or-dependant-v3']
+    if (contacted == "yes"){
+        res.redirect('/v3/dependant-details2')
+    }
+    else {
+        res.redirect('/v3/nhs-question')
+    }
+
+})
+
 
 module.exports = router;
