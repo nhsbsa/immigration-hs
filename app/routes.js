1111e7e002da1025a6ba6a1b8795f5526fd8564d
Tom Leeds <thomas.leeds@nhs.net>
2023-02-27 09:35:30 +0000

    Updates to older prototype versions

--- app/routes.js
+++ app/routes.js
@@ -2713,7 +2713,7 @@ router.post('/non-nhs-path-another-job-v14', function(req,res){
         res.redirect('/v14/non-nhs-path-another-job')
     }
     else {
-        res.redirect('/v14/upload-of-contract')
+        res.redirect('/v14/upload-of-payslips')
     }
 
 })
